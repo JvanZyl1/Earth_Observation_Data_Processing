885dcae1d18e4c7c62e4a1ae00e9c7529fe35d16
luss <lucia.soto@deimos-space.com>
2021-10-07 18:08:35 +0200

    Changing from Virtual Machine based to Windows based

--- l1b/mainL1b.py
+++ l1b/mainL1b.py
@@ -4,11 +4,13 @@
 from l1b.src.l1b import l1b
 
 # Directory - this is the common directory for the execution of the E2E, all modules
-auxdir = '/home/luss/EODP/Earth_Observation_Data_Processing/auxiliary'
+auxdir = r'C:/Users/Jonathan van Zyl/Documents/BSC Aerospace Engineering/TU Delft_UC3M Yr.3/UC3M Exchange/Earth Observation and Data Processing/VM_shared_folder/eodp_students-master\/auxiliary'
 #auxdir = '/home/luss/EODP/eodp/auxiliary'
-indir = '/home/luss/my_shared_folder/EODP_TER_2021/EODP-TS-L1B/input/'
-outdir = '/home/luss/my_shared_folder/test_l1b/'
+indir = r'C:/Users/Jonathan van Zyl/Documents/BSC Aerospace Engineering/TU Delft_UC3M Yr.3/UC3M Exchange/Earth Observation and Data Processing/EODP_TER_2021/EODP-TS-L1B/input'
+outdir = r'C:/Users/Jonathan van Zyl/Documents/BSC Aerospace Engineering/TU Delft_UC3M Yr.3/UC3M Exchange/Earth Observation and Data Processing/VM_shared_folder/test_l1b'
 
 # Initialise the ISM
 myL1b = l1b(auxdir, indir, outdir)
 myL1b.processModule()
+
+
---
+++ l1b/test/test_l1b.py
@@ -0,0 +1,52 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
+# Import read toa from the write toa file under common io
+# Directory is the my_shared_folder/EODP.../ed...
+
+from common.io.writeToa import readToa
+import numpy as np
+import math
+
+# Read the toa's from the import - turn this into a loop
+toa_0 = readToa('C:/Users/Jonathan van Zyl/Documents/BSC Aerospace Engineering/TU Delft_UC3M Yr.3/UC3M Exchange/Earth Observation and Data Processing/VM_shared_folder/test_l1b', 'l1b_toa_eq_VNIR-0.nc') #Perhaps .nc
+toa_0_in = readToa(r'C:/Users/Jonathan van Zyl/Documents/BSC Aerospace Engineering/TU Delft_UC3M Yr.3/UC3M Exchange/Earth Observation and Data Processing/VM_shared_folder/EODP_TER_2021/EODP-TS-L1B/input', 'ism_toa_VNIR-0.nc')
+toa_1 = readToa('C:/Users/Jonathan van Zyl/Documents/BSC Aerospace Engineering/TU Delft_UC3M Yr.3/UC3M Exchange/Earth Observation and Data Processing/VM_shared_folder/test_l1b', 'l1b_toa_eq_VNIR-1.nc') #Perhaps .nc
+toa_1_in = readToa(r'C:/Users/Jonathan van Zyl/Documents/BSC Aerospace Engineering/TU Delft_UC3M Yr.3/UC3M Exchange/Earth Observation and Data Processing/VM_shared_folder/EODP_TER_2021/EODP-TS-L1B/input', 'ism_toa_VNIR-1.nc')
+toa_2 = readToa('C:/Users/Jonathan van Zyl/Documents/BSC Aerospace Engineering/TU Delft_UC3M Yr.3/UC3M Exchange/Earth Observation and Data Processing/VM_shared_folder/test_l1b', 'l1b_toa_eq_VNIR-2.nc') #Perhaps .nc
+toa_2_in = readToa(r'C:/Users/Jonathan van Zyl/Documents/BSC Aerospace Engineering/TU Delft_UC3M Yr.3/UC3M Exchange/Earth Observation and Data Processing/VM_shared_folder/EODP_TER_2021/EODP-TS-L1B/input', 'ism_toa_VNIR-2.nc')
+toa_3 = readToa('C:/Users/Jonathan van Zyl/Documents/BSC Aerospace Engineering/TU Delft_UC3M Yr.3/UC3M Exchange/Earth Observation and Data Processing/VM_shared_folder/test_l1b', 'l1b_toa_eq_VNIR-3.nc') #Perhaps .nc
+toa_3_in = readToa(r'C:/Users/Jonathan van Zyl/Documents/BSC Aerospace Engineering/TU Delft_UC3M Yr.3/UC3M Exchange/Earth Observation and Data Processing/VM_shared_folder/EODP_TER_2021/EODP-TS-L1B/input', 'ism_toa_VNIR-3.nc')
+toa_all = [toa_0, toa_1, toa_2, toa_3]
+toa_in = [toa_0_in, toa_1_in, toa_2_in, toa_3_in]
+
+Check_pass = True #assumed it's passed
+for i in range(len(toa_all)):
+    N = 100*150
+    sum_ij = np.sum(toa_all[i])
+    mean = sum_ij/(N)
+    sigma = np.sqrt(np.sum(np.square(toa_all[i] - mean))/N**2)
+    threesigma = 3*sigma
+    difference = np.absolute(np.subtract(toa_all[i], toa_in[i]))
+    percentage_diff_matrix = np.divide(difference, toa_0_in)*100
+    counter = 0
+    for row in percentage_diff_matrix:
+        for elem in row:
+            if elem < 0.01:
+                counter += 1
+    check = False
+    if counter > threesigma:
+        check = True
+    else:
+        Check_pass = False
+if Check_pass == True:
+    print("Question 1 is satisfied")
+
+# Carry out the test tasks
+#1. Find the Mean
+    #mean = sum(radiance(i,j))/(no.pixels**2)
+#2. Find the Standard Deivation
+    #sigma = math.sqrt((sum(radiance(i,j) - mean))**2)/no.pixels
+#3. Check sigma of each bank is <0.01% away for all points
+#Link this python file to the main file so that it will run properly.
