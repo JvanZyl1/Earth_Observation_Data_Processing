b97cdf868ae7a6544379b49a3c5411dfd6bb036d
luss <lucia.soto@deimos-space.com>
2021-10-25 14:08:57 +0200

    Correction of the l1b test

--- l1b/test_l1b_2.py
+++ l1b/test_l1b_2.py
@@ -30,10 +30,48 @@ band = ['VNIR-0', 'VNIR-1', 'VNIR-2', 'VNIR-3']
 nlines, ncolumns = 100, 150
 # Read the toa's from the import - turn this into a loop
 for i in range(len(band)):
-    toa_input_1 = readToa('/home/luss/my_shared_folder/EODP_TER_2021/EODP-TS-L1B/output', 'l1b_toa_eq_' + band[i] + '.nc')
-    toa_test_1 = readToa('/home/luss/my_shared_folder/test_l1b', 'l1b_toa_eq_' + band[i] + '.nc')
+    toa_input_1 = readToa('/home/luss/my_shared_folder/EODP_TER_2021/EODP-TS-L1B/output', 'l1b_toa_' + band[i] + '.nc')
+    toa_test_1 = readToa('/home/luss/my_shared_folder/test_l1b', 'l1b_toa_' + band[i] + '.nc')
     Check_passed_1 = threesigmacheck(toa_input_1, toa_test_1, nlines, ncolumns)
     if Check_passed_1 == True:
         print("The differences with respect to the output are <0.01% for at least 3-sigma of the points.", str(band[i]))
     else:
         print("Differences are too great")
+
+
+plots = True
+if plots == False:
+    exit(0)
+'''PART 2: Plotting'''
+act = np.arange(1,151,1) #Creates a range of act pixels
+band = ['VNIR-0', 'VNIR-1', 'VNIR-2', 'VNIR-3']
+nlines, ncolumns = 100, 150
+isrf_row, l1b_row = [], []
+# Read the toa's from the import - turn this into a loop
+for i in range(len(band)):
+    toa_isrf = readToa('/home/luss/my_shared_folder/EODP_TER_2021/EODP-TS-L1B/input', 'ism_toa_isrf_' + band[i] + '.nc')
+    #toa_restored = readToa('/home/luss/my_shared_folder/EODP_TER_2021/EODP-TS-L1B/output', 'l1b_toa_' + band[i] + '.nc')
+    toa_restored = readToa('/home/luss/my_shared_folder/test_l1b', 'l1b_toa_' + band[i] + '.nc')
+    isrf_row.append(np.sum(toa_isrf, axis=0))
+    l1b_row.append(np.sum(toa_restored, axis=0))
+    print(np.shape(l1b_row))
+
+#Plotting of ism_toa_isrf and l1b_toa for each band with act_pixel vs. toa
+fig, axs = plt.subplots(2, 2)
+axs[0, 0].plot(act, isrf_row[0], 'tab:blue', act, l1b_row[0], 'tab:orange')
+axs[0, 0].set_title('VNIR-0')
+axs[0, 1].plot(act, isrf_row[1], 'tab:blue', act, l1b_row[1], 'tab:orange')
+axs[0, 1].set_title('VNIR-1')
+axs[1, 0].plot(act, isrf_row[2], 'tab:blue', act, l1b_row[2], 'tab:orange')
+axs[1, 0].set_title('VNIR-2')
+axs[1, 1].plot(act, isrf_row[3], 'tab:blue', act, l1b_row[3], 'tab:orange')
+axs[1, 1].set_title('VNIR-3')
+
+for ax in axs.flat:
+    ax.set(xlabel='Act Pixel [-]', ylabel='TOA [mW/m2/sr]')
+
+# Hide x labels and tick labels for top plots and y ticks for right plots.
+for ax in axs.flat:
+    ax.label_outer()
+
+plt.show()
